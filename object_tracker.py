845ddc5527cadc5e1a58254a7b84946315f6d1b9
Francois Cresson <francois.cresson@isen.yncrea.fr>
2020-07-06 16:32:29 +0200

    Object + server

--- object_tracker.py
+++ object_tracker.py
@@ -18,89 +18,61 @@ from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
 from PIL import Image
 
-flags.DEFINE_string('classes', './data/labels/coco.names', 'path to classes file')
-flags.DEFINE_string('weights', './weights/yolov3.tf',
-                    'path to weights file')
-flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
-flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_string('video', './data/video/test.mp4',
-                    'path to video file or number for webcam)')
-flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
-flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
 
 
-def main(_argv):
-    # Definition of the parameters
-    max_cosine_distance = 0.5
-    nn_budget = None
-    nms_max_overlap = 1.0
+class ObjectTracker():
     
-    #initialize deep sort
-    model_filename = 'model_data/mars-small128.pb'
-    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
+    def __init__(self):
+        # Definition of the parameters
+        self.max_cosine_distance = 0.5
+        self.nn_budget = None
+        self.nms_max_overlap = 1.0
+        
+        #initialize deep sort
+        self.model_filename = 'model_data/mars-small128.pb'
+        self.encoder = gdet.create_box_encoder(self.model_filename, batch_size=1)
+        self.metric = nn_matching.NearestNeighborDistanceMetric("cosine", self.max_cosine_distance, self.nn_budget)
+        self.tracker = Tracker(self.metric)
 
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    if len(physical_devices) > 0:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        self.physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        if len(self.physical_devices) > 0:
+            tf.config.experimental.set_memory_growth(self.physical_devices[0], True)
 
-    if FLAGS.tiny:
-        yolo = YoloV3Tiny(classes=FLAGS.num_classes)
-    else:
-        yolo = YoloV3(classes=FLAGS.num_classes)
+        if FLAGS.tiny:
+            self.yolo = YoloV3Tiny(classes=FLAGS.num_classes)
+        else:
+            self.yolo = YoloV3(classes=FLAGS.num_classes)
 
-    yolo.load_weights(FLAGS.weights)
-    logging.info('weights loaded')
+        self.yolo.load_weights(FLAGS.weights)
+        logging.info('weights loaded')
 
-    class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
-    logging.info('classes loaded')
+        self.class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
+        logging.info('classes loaded')
 
-    try:
-        vid = cv2.VideoCapture(int(FLAGS.video))
-    except:
-        vid = cv2.VideoCapture(FLAGS.video)
-
-    out = None
-
-    if FLAGS.output:
-        # by default VideoCapture returns float instead of int
-        width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(vid.get(cv2.CAP_PROP_FPS))
-        codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-        out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
-        list_file = open('detection.txt', 'w')
-        frame_index = -1 
+        self.fps = 0.0
+        
+        self.last_tracked = []
     
-    fps = 0.0
-    count = 0 
-    while True:
-        _, img = vid.read()
-
+    def run(self, img):
         if img is None:
             logging.warning("Empty Frame")
             time.sleep(0.1)
-            count+=1
-            if count < 3:
-                continue
-            else: 
-                break
+            return
 
         img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
         img_in = tf.expand_dims(img_in, 0)
         img_in = transform_images(img_in, FLAGS.size)
 
         t1 = time.time()
-        boxes, scores, classes, nums = yolo.predict(img_in)
+        boxes, scores, classes, nums = self.yolo.predict(img_in)
         classes = classes[0]
         names = []
         for i in range(len(classes)):
-            names.append(class_names[int(classes[i])])
+            names.append(self.class_names[int(classes[i])])
+
         names = np.array(names)
         converted_boxes = convert_boxes(img, boxes[0])
-        features = encoder(img, converted_boxes)    
+        features = self.encoder(img, converted_boxes)    
         detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(converted_boxes, scores[0], names, features)]
         
         #initialize color map
@@ -111,55 +83,39 @@ def main(_argv):
         boxs = np.array([d.tlwh for d in detections])
         scores = np.array([d.confidence for d in detections])
         classes = np.array([d.class_name for d in detections])
-        indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+        indices = preprocessing.non_max_suppression(boxs, classes, self.nms_max_overlap, scores)
         detections = [detections[i] for i in indices]        
 
         # Call the tracker
-        tracker.predict()
-        tracker.update(detections)
+        self.tracker.predict()
+        self.tracker.update(detections)
 
-        for track in tracker.tracks:
+        for track in self.tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
+                return
             bbox = track.to_tlbr()
             class_name = track.get_class()
             color = colors[int(track.track_id) % len(colors)]
             color = [i * 255 for i in color]
+            self.last_tracked.append((track.track_id, track.get_class(), int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])))
             cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
             cv2.rectangle(img, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
             cv2.putText(img, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
-            
+        
+        
         ### UNCOMMENT BELOW IF YOU WANT CONSTANTLY CHANGING YOLO DETECTIONS TO BE SHOWN ON SCREEN
         #for det in detections:
         #    bbox = det.to_tlbr() 
         #    cv2.rectangle(img,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
         
         # print fps on screen 
-        fps  = ( fps + (1./(time.time()-t1)) ) / 2
-        cv2.putText(img, "FPS: {:.2f}".format(fps), (0, 30),
+        self.fps  = ( self.fps + (1./(time.time()-t1)) ) / 2
+        cv2.putText(img, "FPS: {:.2f}".format(self.fps), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-        cv2.imshow('output', img)
-        if FLAGS.output:
-            out.write(img)
-            frame_index = frame_index + 1
-            list_file.write(str(frame_index)+' ')
-            if len(converted_boxes) != 0:
-                for i in range(0,len(converted_boxes)):
-                    list_file.write(str(converted_boxes[i][0]) + ' '+str(converted_boxes[i][1]) + ' '+str(converted_boxes[i][2]) + ' '+str(converted_boxes[i][3]) + ' ')
-            list_file.write('\n')
+        
+    def get_last_tracked(self):
+        return self.last_tracked    
 
-        # press q to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
-    vid.release()
-    if FLAGS.ouput:
-        out.release()
-        list_file.close()
-    cv2.destroyAllWindows()
+    def clear_last_tracked(self):
+        self.last_tracked.clear()
 
-
-if __name__ == '__main__':
-    try:
-        app.run(main)
-    except SystemExit:
-        pass
